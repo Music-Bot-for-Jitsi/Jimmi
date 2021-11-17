09d3749d37d98dd65f758e9a8460361a7a26ce56
Philipp Fruck <dev@p-fruck.de>
2021-11-17 14:24:21 +0100

    added footer component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,25 +1,27 @@
 <script lang="ts">
-  import { isLoading } from 'svelte-i18n'
-  import Router from 'svelte-spa-router'
-  import { wrap } from 'svelte-spa-router/wrap'
-  import { setupI18n } from './i18n'
-  import Home from './routes/Home.svelte'
-  import NotFound from './routes/NotFound.svelte'
-  import "./tailwind.css"
+  import { isLoading } from "svelte-i18n";
+  import Router from "svelte-spa-router";
+  import { wrap } from "svelte-spa-router/wrap";
+  import { setupI18n } from "./i18n";
+  import Home from "./routes/Home.svelte";
+  import NotFound from "./routes/NotFound.svelte";
+  import Footer from "./components/Footer.svelte";
+  import "./tailwind.css";
 
   setupI18n();
 
   const routes = {
-    '/': Home,
-    '/bot/:instance/:room': wrap({
-      asyncComponent: () => import('./routes/Bot.svelte'), // async import to reduce bundle size
+    "/": Home,
+    "/bot/:instance/:room": wrap({
+      asyncComponent: () => import("./routes/Bot.svelte"), // async import to reduce bundle size
     }),
-    '*': NotFound, // Catch-All
-  }
+    "*": NotFound, // Catch-All
+  };
 </script>
 
 <main class="h-screen">
   {#if !$isLoading}
-    <Router {routes}/>
+    <Router {routes} />
   {/if}
+  <Footer />
 </main>
