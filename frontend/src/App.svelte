62e4b97abc7cda958540f9e3113284b3bfbde5ba
Philipp Fruck <dev@p-fruck.de>
2022-02-15 19:34:03 +0100

    init frontend
    Co-authored-by: Simon Walz <57764295+Simon-Walz@users.noreply.github.com>
    Co-authored-by: Tjarbo <16938041+tjarbo@users.noreply.github.com>

---
+++ frontend/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Home from "@/components/Home.svelte";
+  import { fade } from "svelte/transition";
+
+  let name = "World";
+</script>
+
+<main transition:fade>
+  <Home {name} />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ frontend/src/components/Home.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let name;
+</script>
+
+<h1>Hello {name}!</h1>
+<p>
+  Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how
+  to build Svelte apps.
+</p>
+
+<link
+  href="https://fonts.googleapis.com/css?family=Overpass:100,400"
+  rel="stylesheet"
+/>
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
