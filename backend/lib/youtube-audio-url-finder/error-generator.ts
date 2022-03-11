e0f93f4048efa9ed26c7c0c564268bbbf7148765
Simon-Walz <57764295+Simon-Walz@users.noreply.github.com>
2022-03-11 15:51:56 +0100

    add error handling enum and named-error generator

---
+++ backend/lib/youtube-audio-url-finder/error-generator.ts
@@ -0,0 +1,25 @@
+export default class ErrorGenerator {
+  /**
+   * Generates an error object with given name
+   * @param name The name to generate the error with
+   * @returns The generated error
+   */
+  createNamedError(name: string): Error {
+    const error: Error = new Error();
+    error.name = name;
+    return error;
+  }
+
+  /**
+   * Generates an error object with given name and message
+   * @param name The name to generate the error with
+   * @param message The message to generate the error with
+   * @returns The generated error
+   */
+  createNamedErrorWithMessage(name: string, message: string): Error {
+    const error: Error = new Error();
+    error.name = name;
+    error.message = message;
+    return error;
+  }
+}
