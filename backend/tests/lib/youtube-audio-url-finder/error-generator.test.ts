149060021e74f0b0f83c653a0343b9f3172998f8
Simon-Walz <57764295+Simon-Walz@users.noreply.github.com>
2022-03-11 16:18:30 +0100

    add unit test files and first unit tests for error generator

---
+++ backend/tests/lib/youtube-audio-url-finder/error-generator.test.ts
@@ -0,0 +1,28 @@
+import { assertEquals } from "std/testing/asserts.ts";
+import ErrorGenerator from "../../../lib/youtube-audio-url-finder/error-generator.ts";
+
+Deno.test(function testCreateNamedError() {
+  const name = "testName";
+  const errorGenerator: ErrorGenerator = new ErrorGenerator();
+  const testError: Error = errorGenerator.createNamedError(name);
+  const anotherError = new Error();
+  anotherError.name = name;
+  assertEquals(testError.name, name);
+  assertEquals(testError, anotherError);
+});
+
+Deno.test(function testCreateNamedErrorWithMessage() {
+  const name = "testName";
+  const message = "testMessage";
+  const errorGenerator: ErrorGenerator = new ErrorGenerator();
+  const testError: Error = errorGenerator.createNamedErrorWithMessage(
+    name,
+    message,
+  );
+  const anotherError = new Error();
+  anotherError.name = name;
+  anotherError.message = message;
+  assertEquals(testError.name, name);
+  assertEquals(testError.message, message);
+  assertEquals(testError, anotherError);
+});
--- backend/tests/lib/youtube-audio-url-finder/mod.test.ts
+++
@@ -1,4 +0,0 @@
-import { assertEquals } from "std/testing/asserts.ts";
-import { Stub, stub } from "mock";
-
-import YoutubeAudioUrlFinder from "../../../lib/youtube-audio-url-finder/mod.ts";
