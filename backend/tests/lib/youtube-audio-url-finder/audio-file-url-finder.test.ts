7db422ab72992882e8765330b0ad9d6506cb1b94
Simon-Walz <57764295+Simon-Walz@users.noreply.github.com>
2022-03-18 10:55:25 +0100

    add more unit testing

--- backend/tests/lib/youtube-audio-url-finder/audio-file-url-finder.test.ts
+++ backend/tests/lib/youtube-audio-url-finder/audio-file-url-finder.test.ts
@@ -2,4 +2,65 @@ import { assertEquals, assertThrows } from 'std/testing/asserts.ts';
 import { Stub, stub } from 'mock/mod.ts';
 import { assertSpyCall } from 'mock/mod.ts';
 import AudioFileUrlFinder from '../../../lib/youtube-audio-url-finder/audio-file-url-finder.ts';
+import { AudioFileData } from '../../../lib/youtube-audio-url-finder/invidious.interfaces.ts';
+import { testAudioFileData } from './test-audio-file-data.ts';
 import { Errors } from '../../../lib/youtube-audio-url-finder/errors.ts';
+
+Deno.test(function testConstructor() {
+  const audioFileUrlFinder: AudioFileUrlFinder = new AudioFileUrlFinder();
+  assertEquals(audioFileUrlFinder['invidiousVideoUrl'], '');
+});
+
+Deno.test(function testSetInvidiousVideoUrl() {
+  const testInvidiousVideoUrl = 'https://sometestinvidiousvideourl';
+  const audioFileUrlFinder: AudioFileUrlFinder = new AudioFileUrlFinder();
+
+  audioFileUrlFinder.setInvidiousVideoUrl(testInvidiousVideoUrl);
+
+  assertEquals(audioFileUrlFinder['invidiousVideoUrl'], testInvidiousVideoUrl);
+});
+
+Deno.test(async function testFindAudioFileUrl() {
+  const testAudioFileUrl = 'https://sometestaudiofileourl';
+  const audioFileUrlFinder: AudioFileUrlFinder = new AudioFileUrlFinder();
+
+  const _findInvidiousInstanceUrl: Stub<AudioFileUrlFinder> = stub(
+    audioFileUrlFinder,
+    'fetchAudioFileData',
+    () => {
+      return testAudioFileData;
+    },
+  );
+
+  const extractAudioFileUrl: Stub<AudioFileUrlFinder> = stub(
+    audioFileUrlFinder,
+    'extractAudioFileUrl',
+    () => {
+      return testAudioFileUrl;
+    },
+  );
+
+  assertEquals(await audioFileUrlFinder.findAudioFileUrl(), testAudioFileUrl);
+  assertSpyCall(extractAudioFileUrl, 0, {
+    args: [testAudioFileData],
+    self: audioFileUrlFinder,
+    returned: testAudioFileUrl,
+  });
+});
+
+Deno.test(function testExtractAudioFileUrl() {
+  const expectedUrl = testAudioFileData['adaptiveFormats'][0].url;
+  const testEmptyAudioFileData: AudioFileData = { adaptiveFormats: [] };
+  const audioFileUrlFinder: AudioFileUrlFinder = new AudioFileUrlFinder();
+  assertEquals(
+    audioFileUrlFinder['extractAudioFileUrl'](testAudioFileData),
+    expectedUrl,
+  );
+
+  assertThrows(
+    () => audioFileUrlFinder['extractAudioFileUrl'](testEmptyAudioFileData),
+    (error: Error) => {
+      assertEquals(error.name, Errors.NO_SUITABLE_ADAPTIVE_FORMATS);
+    },
+  );
+});
---
+++ backend/tests/lib/youtube-audio-url-finder/test-audio-file-data.ts
@@ -0,0 +1,33 @@
+import { AudioFileData } from '../../../lib/youtube-audio-url-finder/invidious.interfaces.ts';
+
+export const testAudioFileData: AudioFileData = {
+  adaptiveFormats: [{
+    'index': '641-924',
+    'bitrate': '49938',
+    'init': '0-640',
+    'url':
+      'https://rr1---sn-5goeen7y.googlevideo.com/videoplayback?expire=1647616216&ei=eEw0Yqb2M-OBp-oP39Gk4Ag&ip=95.216.24.230&id=o-AMdWvqig0BI0hURUSC0xyIdTbx0QrRysNcKtikStWCD4&itag=139&source=youtube&requiressl=yes&mh=s5&mm=31%2C29&mn=sn-5goeen7y%2Csn-5go7yne6&ms=au%2Crdu&mv=m&mvi=1&pl=24&initcwndbps=365000&vprv=1&mime=audio%2Fmp4&gir=yes&clen=1248418&dur=204.614&lmt=1646764485469070&mt=1647594296&fvip=2&keepalive=yes&fexp=24001373%2C24007246&c=ANDROID&txp=5532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRQIgBzLbcD-wI0vlKfUnWK8XrNm9kbH70SWs4S_Ry9GGp0sCIQCvCj68lu_-7piWgz-PpBug4pYzbtck6uZqMz_ILEYgPg%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhALsmXMWQ4mlQA-6zhZ-3mUvWXlpoRAqW1EK32aiE_DyHAiBdxJu9V1lZWsm0iUI9oQzeQamH38-3TA2fMDdFwPq04Q%3D%3D&host=rr1---sn-5goeen7y.googlevideo.com',
+    'itag': '139',
+    'type': 'audio/mp4; codecs="mp4a.40.5"',
+    'clen': '1248418',
+    'lmt': '1646764485469070',
+    'projectionType': 'RECTANGULAR',
+    'fps': 30,
+    'container': 'm4a',
+    'encoding': 'aac',
+  }, {
+    'index': '632-915',
+    'bitrate': '130442',
+    'init': '0-631',
+    'url':
+      'https://rr1---sn-5goeen7y.googlevideo.com/videoplayback?expire=1647616216&ei=eEw0Yqb2M-OBp-oP39Gk4Ag&ip=95.216.24.230&id=o-AMdWvqig0BI0hURUSC0xyIdTbx0QrRysNcKtikStWCD4&itag=140&source=youtube&requiressl=yes&mh=s5&mm=31%2C29&mn=sn-5goeen7y%2Csn-5go7yne6&ms=au%2Crdu&mv=m&mvi=1&pl=24&initcwndbps=365000&vprv=1&mime=audio%2Fmp4&gir=yes&clen=3311049&dur=204.544&lmt=1646764485527740&mt=1647594296&fvip=2&keepalive=yes&fexp=24001373%2C24007246&c=ANDROID&txp=5532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRAIgaxMFz2uUPbKtPZ09RAUvQ8dwpXZMB9S7roYmA8SovPcCIFHHXf4Ip6nEllpVQ1B4j20dZZf-Zs3khOISIPGNsQcV&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhALsmXMWQ4mlQA-6zhZ-3mUvWXlpoRAqW1EK32aiE_DyHAiBdxJu9V1lZWsm0iUI9oQzeQamH38-3TA2fMDdFwPq04Q%3D%3D&host=rr1---sn-5goeen7y.googlevideo.com',
+    'itag': '140',
+    'type': 'audio/mp4; codecs="mp4a.40.2"',
+    'clen': '3311049',
+    'lmt': '1646764485527740',
+    'projectionType': 'RECTANGULAR',
+    'fps': 30,
+    'container': 'm4a',
+    'encoding': 'aac',
+  }],
+};
