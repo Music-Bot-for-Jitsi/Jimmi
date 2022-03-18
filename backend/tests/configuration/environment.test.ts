b19ff2d67bd23307d538426678d720169a87f7e5
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2022-03-18 10:14:34 +0100

    Extract config variable

--- backend/tests/configuration/environment.test.ts
+++ backend/tests/configuration/environment.test.ts
@@ -2,13 +2,14 @@ import { assert, assertEquals, assertExists, assertThrows } from 'std/testing/as
 import { DotenvConfig } from 'https://deno.land/x/dotenv@v3.2.0/mod.ts';
 import { envVarsSchema } from '../../src/configuration/environment.ts';
 
+const defaultConfig: DotenvConfig = {
+  PORT: '8000',
+  HOSTNAME: 'localhost',
+  FRONTEND_DIR: 'frontend',
+};
+
 Deno.test('if config validation works', () => {
-  const config: DotenvConfig = {
-    PORT: '8000',
-    HOSTNAME: 'localhost',
-    FRONTEND_DIR: 'frontend',
-  };
-  const { error, value: envVars } = envVarsSchema.validate(config);
+  const { error, value: envVars } = envVarsSchema.validate(defaultConfig);
   assert(error === undefined);
   assertExists(envVars);
   assertEquals(envVars.PORT, 8000);
@@ -19,9 +20,8 @@ Deno.test('if config validation works', () => {
 Deno.test('if config validation error works', () => {
   assertThrows(() => {
     const config: DotenvConfig = {
+      ...defaultConfig,
       PORT: '90000',
-      HOSTNAME: 'localhost',
-      FRONTEND_DIR: 'frontend',
     };
     const { error } = envVarsSchema.validate(config);
     if (error) throw new Error(`Config validation error: ${error?.message}`);
