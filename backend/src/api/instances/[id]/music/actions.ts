16c1099075bbb3e100e8cfd2d108575f07f3cee6
Simon-Walz <57764295+Simon-Walz@users.noreply.github.com>
2022-03-20 22:31:04 +0100

    add /api/instances/[id]/music endpoints

---
+++ backend/src/api/instances/[id]/music/actions.ts
@@ -0,0 +1,12 @@
+export enum Actions {
+  PAUSE = 'pause',
+  PLAY = 'play',
+  STOP = 'stop',
+}
+
+export enum ActionMessages {
+  PAUSE = 'paused music stream',
+  PLAY = 'started playing music stream',
+  STOP = 'stopped music stream',
+  UNKNOWN = 'unknown action',
+}
