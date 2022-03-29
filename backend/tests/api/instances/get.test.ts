656f3b436a9eb0e418e3f45681259d7a6b4afe30
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2022-03-29 19:06:24 +0200

    Fix async test race conditions

--- backend/tests/api/instances/get.test.ts
+++ backend/tests/api/instances/get.test.ts
@@ -1,12 +1,11 @@
 import { superdeno } from 'superdeno/mod.ts';
 import app from '../../import.app.symlink.ts';
+import { assertEquals, assertInstanceOf, assertStringIncludes } from 'std/testing/asserts.ts';
 
 Deno.test('GET /api/instances that it returns valid JSON!', async () => {
-  await superdeno(app)
-    .get('/api/instances')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .then((res) => {
-      if (!(res.body instanceof Array)) throw new Error('res.body should be array');
-    });
+  const res = await superdeno(app).get('/api/instances');
+
+  assertEquals(res.statusCode, 200);
+  assertStringIncludes(res.headers['content-type'] as string, 'json');
+  assertInstanceOf(res.body, Array, 'res.body should be array');
 });
