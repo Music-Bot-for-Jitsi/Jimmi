478f01c62e0de968cdbb7224df30d22f39232d4c
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2022-03-29 11:28:00 +0200

    Add more tests

--- backend/tests/api/instances/[id]/get.test.ts
+++ backend/tests/api/instances/[id]/get.test.ts
@@ -1,4 +1,5 @@
 import { superdeno } from 'superdeno/mod.ts';
+import { createJimmi } from '../../../../src/service/Jimmi.service.ts';
 import app from '../../../import.app.symlink.ts';
 
 Deno.test('GET /api/instances/unknown-id that it returns 404!', async () => {
@@ -6,4 +7,16 @@ Deno.test('GET /api/instances/unknown-id that it returns 404!', async () => {
     .get('/api/instances/unknown-id')
     .expect('Content-Type', /text/)
     .expect(404);
+});
+
+Deno.test('GET /api/instances/id that it returns the id of the created instance', async () => {
+  const jimmi = await createJimmi();
+  await superdeno(app)
+    .post(`/api/instances/${jimmi.id}`)
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .then((res) => {
+      if (!(Object.keys(res.body).includes('id'))) throw new Error('res.body should include id');
+      if (res.body.id !== jimmi.id) throw new Error('id should match created jimmi instance');
+    });
 });
